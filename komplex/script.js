da4e1c6938e5ee6b24ac05e2a0c4b7ebdcf75c9d
FeketeD <113105668+FeketeD@users.noreply.github.com>
2023-10-11 23:25:11 +0200

    Small changes

---
+++ komplex/script.js
@@ -0,0 +1,9 @@
+/*
+document.getElementById("userIcon").onmouseover = function(){
+    document.getElementById("userIcon").classList.add("fa-shake")
+}
+document.getElementById("userIcon").onmouseleave = function(){
+    document.getElementById("userIcon").classList.remove("fa-shake")
+}
+*/
+
