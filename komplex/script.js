dee727da9399d5a683f7bcb117144bca559b174b
FeketeD <113105668+FeketeD@users.noreply.github.com>
2023-10-15 23:50:33 +0200

    bug fixes, features added

--- komplex/script.js
+++ komplex/script.js
@@ -1,11 +1,11 @@
-/*
-document.getElementById("userIcon").onmouseover = function(){
-    document.getElementById("userIcon").classList.add("fa-shake")
+let click = 0
+document.getElementById('logo').onclick = function(){
+    click++
+    if (click == 10) {
+        alert("Made by: Dániel Fekete, Máté Balázs")
+        click = 0
+    }
 }
-document.getElementById("userIcon").onmouseleave = function(){
-    document.getElementById("userIcon").classList.remove("fa-shake")
-}
-*/
 
 document.getElementById("signup").onsubmit = function(event){
     
@@ -15,7 +15,6 @@ document.getElementById("signup").onsubmit = function(event){
     let age = document.getElementById("age")
     let gender = document.getElementById("gender")
     let profession = document.getElementById("profession")
-    //let sector = document.getElementById("sector")
     let sector = document.getElementsByName('sector')
     let selectedSectors = []
 
@@ -27,7 +26,7 @@ document.getElementById("signup").onsubmit = function(event){
 
 
     let letters = (/^[A-Za-z]+$/)
-    const emailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    const emailValidation = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(?:com|hu|net|edu)$/i
 
     //Name validation
     if (name.value != "" && name.value.match(letters) && name.value[0] === name.value[0].toUpperCase()) {
@@ -60,6 +59,67 @@ document.getElementById("signup").onsubmit = function(event){
         }, 900)
     }
 
+    //Password validation
+    let passwordBox = document.getElementById('passwordBox')
+    if (password.value != "") {
+        passwordBox.classList.add('is-valid')
+        passwordBox.classList.remove('is-invalid')
+    }
+    else{
+        event.preventDefault()
+        passwordBox.classList.remove('is-valid')
+        passwordBox.classList.add('is-invalid')
+        document.getElementById("passwordIcon").classList.add("fa-shake")
+        setTimeout(function(){
+            document.getElementById("passwordIcon").classList.remove("fa-shake")
+        }, 900)
+    }
+
+    //Age validation
+    if (age.value != "") {
+        age.classList.add('is-valid')
+        age.classList.remove('is-invalid')
+    }
+    else{
+        event.preventDefault()
+        age.classList.remove('is-valid')
+        age.classList.add('is-invalid')
+        document.getElementById("ageIcon").classList.add("fa-shake")
+        setTimeout(function(){
+            document.getElementById("ageIcon").classList.remove("fa-shake")
+        }, 900)
+    }
+
+    //Gender validation
+    if (gender.value != "") {
+        gender.classList.add('is-valid')
+        gender.classList.remove('is-invalid')
+    }
+    else{
+        event.preventDefault()
+        gender.classList.remove('is-valid')
+        gender.classList.add('is-invalid')
+        document.getElementById("genderIcon").classList.add("fa-shake")
+        setTimeout(function(){
+            document.getElementById("genderIcon").classList.remove("fa-shake")
+        }, 900)
+    }
+
+    //Profession validation
+    if (profession.value != "") {
+        profession.classList.add('is-valid')
+        profession.classList.remove('is-invalid')
+    }
+    else{
+        event.preventDefault()
+        profession.classList.remove('is-valid')
+        profession.classList.add('is-invalid')
+        document.getElementById("profIcon").classList.add("fa-shake")
+        setTimeout(function(){
+            document.getElementById("profIcon").classList.remove("fa-shake")
+        }, 900)
+    }
+
     var body = JSON.stringify({
         name: name.value,
         email: email.value,
@@ -70,10 +130,13 @@ document.getElementById("signup").onsubmit = function(event){
         sector: selectedSectors
     }, null, 2)
     console.log(body)
+    
+    
 }
 
 document.getElementById("passwordIcon").onclick = function(){
     let lock = document.getElementById("passwordIcon")
+    
     //Password generation
     let passwordLength = 16;
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789---";
@@ -91,5 +154,26 @@ document.getElementById("passwordIcon").onclick = function(){
         password.disabled = true
         password.value = pass
     }
-    
+}
+
+document.getElementById('passwordToggle').onclick = function(){
+    let check = document.getElementById('passwordToggle')
+    if (password.type == "password") {
+        password.type = "text"
+        check.classList.replace("fa-eye-slash", "fa-eye")
+    }
+    else{
+        password.type = "password"
+        check.classList.replace("fa-eye", "fa-eye-slash")
+    }
+}
+
+function valami(){
+    let box = document.getElementById('passwordBox')
+    box.style.boxShadow = "0 0 0 0 transparent"
+}
+
+document.getElementById('password').onfocus = function(){
+    let box = document.getElementById('passwordBox')
+    box.style.boxShadow = "0 0 0 0.25rem rgba(0, 0, 255, 0.249)"
 }
